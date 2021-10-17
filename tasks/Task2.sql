c575c8c94848962e0b0c47186f01be7c51d9518a
LLAAKMANN <92632241+LLAAKMANN@users.noreply.github.com>
2021-10-17 11:38:24 +0100

    Update Task2.sql
    When running - I realized the initial query is somewhat off:
    
    Use fancy_company;
    
    Select o.order_id as Order_Nbr, UPPER(u.user_first_name) as First_Name, UPPER(u.user_last_name) as Last_name, u.user_id as Customer_ID, i.Sales_amount as Revenue,
    Hour(o.created_at) as Hour,
    Month(o.created_at) as Month,
    Year(o.created_at) as Year
    From orders o
    Join user u 
    On o.user_id = u.user_id
    Join invoice i
    On o.invoice_id = i.invoice_id
    Order by Customer_id asc;
    
    ____________________________________________________
    Another proof -check the revenue for user 1 = Peter Dammit: 
    
    Use fancy_company;
    
    Select u.user_id, Round(Sum(i.sales_amount),2) from User u
    Join orders o on u.user_id = o.user_id
    Join invoice i on o.invoice_id = i.invoice_id
    Where u.user_id = 1
    Group by u.user_id;
    
    
    Result: 543.91

--- tasks/Task2.sql
+++ tasks/Task2.sql
@@ -1,9 +1,11 @@
 -- *******************************************************QUERIES***********************************************************************************************************
 -- Exercise 2 *************************************************************************************************************************************************************
 
-SELECT concat(U.user_first_name," ", U.user_last_name) as Name, Round(Sum(I.sales_amount),2) as Revenue from User as U
-Join orders as O on U.User_ID = O.Order_ID
-Join invoice as I on O.Order_ID = I.Order_ID
-GROUP BY U.User_First_Name, U.User_Last_Name, I.Sales_Amount
-ORDER BY I.Sales_Amount desc
-Limit 3;
+
+SELECT concat (U.user_first_name," ", U.user_last_name) as Name, Round(SUM(i.Sales_amount),2) as Revenue
+From orders o
+Join user u On o.user_id = u.user_id
+Join invoice i On o.invoice_id = i.invoice_id
+GROUP BY Name 
+ORDER BY Revenue DESC
+LIMIT 3;
