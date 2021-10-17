7590177d968b00603df23caff601dd1cb83e168c
LLAAKMANN <92632241+LLAAKMANN@users.noreply.github.com>
2021-10-17 11:44:53 +0100

    Update Task2.sql

--- tasks/Task2.sql
+++ tasks/Task2.sql
@@ -2,7 +2,7 @@
 -- Exercise 2 *************************************************************************************************************************************************************
 
 
-SELECT concat (U.user_first_name," ", U.user_last_name) as Name, Round(SUM(i.Sales_amount),2) as Revenue
+SELECT concat(U.user_first_name," ", U.user_last_name) as Name, Round(SUM(i.Sales_amount),2) as Revenue
 From orders o
 Join user u On o.user_id = u.user_id
 Join invoice i On o.invoice_id = i.invoice_id
