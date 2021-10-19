2a063a18322e5ffa28d7dabc114818580b4719d5
zepedros <a44805@alunos.isel.pt>
2021-10-19 13:49:56 +0100

    swapped AVG to sum when calculatin monthly average

--- tasks/Task3.sql
+++ tasks/Task3.sql
@@ -1,2 +1,2 @@
-Select '01/2019-12/2020' as PeriodOfSales, SUM(Sales_Amount) as TotalSales, Sum(Sales_Amount/2) as YearlyAverage, AVG(Sales_Amount/23) as MonthlyAverage
+Select '01/2019-12/2020' as PeriodOfSales, SUM(Sales_Amount) as TotalSales, Sum(Sales_Amount/2) as YearlyAverage, Sum(Sales_Amount/24) as MonthlyAverage
 from invoice
