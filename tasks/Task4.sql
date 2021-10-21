d0d67ec8a53ae6d55c8eec34601300a765e1ce18
SDeemircan <92641861+SDeemircan@users.noreply.github.com>
2021-10-21 22:53:51 +0100

    Update Task4.sql
    add discount

--- tasks/Task4.sql
+++ tasks/Task4.sql
@@ -1,4 +1,4 @@
-select ua.User_Address_City as location, Round(Sum(sales_amount),2) as sales
+select ua.User_Address_City as location, Round(Sum(sales_amount/(D.Discount_Percentage/100))) as sales
 from user_address as ua
 inner join user as u
 on u.User_ID = ua.User_ID
@@ -6,4 +6,6 @@ inner join orders as o
 on u.user_id = o.user_id
 inner join invoice as i
 on o.order_Id = i.order_id
+left join discount as d
+on o.discount_ID = d.discount_ID
 Group by ua.user_Address_city; 
