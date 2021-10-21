7e8a99ffc4708434f1159479aba38e8888f0812a
SDeemircan <92641861+SDeemircan@users.noreply.github.com>
2021-10-21 22:51:40 +0100

    Update Task3.sql
    add discount

--- tasks/Task3.sql
+++ tasks/Task3.sql
@@ -1,2 +1,6 @@
-Select '01/2019-12/2020' as PeriodOfSales, SUM(Sales_Amount) as TotalSales, Sum(Sales_Amount/2) as YearlyAverage, Sum(Sales_Amount/24) as MonthlyAverage
-from invoice
+Select '01/2019-12/2020' as PeriodOfSales, SUM(Sales_Amount/(D.Discount_Percentage/100)) as TotalSales, Sum(Sales_Amount/(D.Discount_Percentage/100)/2) as YearlyAverage, Sum(Sales_Amount/(D.Discount_Percentage/100)/24) as MonthlyAverage
+from invoice as i
+Join Orders as o 
+ON o.invoice_id = i.invoice_id
+Join Discount as D 
+On o.discount_ID = d.discount_ID
