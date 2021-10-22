cf9e07f8cf90aaee631031faefd94f15b1f9fb3f
SDeemircan <92641861+SDeemircan@users.noreply.github.com>
2021-10-23 00:05:58 +0100

    Update Task2.sql
    updated

--- tasks/Task2.sql
+++ tasks/Task2.sql
@@ -2,10 +2,11 @@
 -- Exercise 2 *************************************************************************************************************************************************************
 
 
-SELECT concat(U.user_first_name," ", U.user_last_name) as Name, Round(SUM(i.sales_amount*((100-d.discount_percentage)/100))) as Revenue
-From orders as o
-Join user  as u On o.user_id = u.user_id
-Join invoice as  i On o.invoice_id = i.invoice_id
-left Join Discount as D On o.discount_ID = d.discount_ID
+SELECT concat(U.user_first_name," ", U.user_last_name) as Name, ROUND(SUM(i.sales_amount* ( 1 - ( d.discount_percentage / 100 ) )),2) as Revenue
+ From orders as o
+ Join user  as u On o.user_id = u.user_id
+ Join invoice as  i On o.invoice_id = i.invoice_id
+ Join Discount as D On o.discount_ID = d.discount_ID
  GROUP BY Name
- ORDER BY Revenue DESC;
+ ORDER BY Revenue DESC
+ LIMIT 3;
