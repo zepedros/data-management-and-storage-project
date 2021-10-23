cc9aa29aefdeddfc7688cb869b4f886ffdd7d4d0
LLAAKMANN <92632241+LLAAKMANN@users.noreply.github.com>
2021-10-23 09:44:58 +0100

    Update Task2.sql
    Limit 3 and round with ,2 missing…

--- tasks/Task2.sql
+++ tasks/Task2.sql
@@ -2,11 +2,11 @@
 -- Exercise 2 *************************************************************************************************************************************************************
 
 USE fancy_company; 
-SELECT concat(U.user_first_name," ", U.user_last_name) as Name, Round(SUM(i.sales_amount*((100-d.discount_percentage)/100))) as Revenue
+SELECT concat(U.user_first_name," ", U.user_last_name) as Name, Round(SUM(i.sales_amount*((100-d.discount_percentage)/100)),2) as Revenue
 FROM Orders as O
 JOIN user  as u On o.user_id = u.user_id
 JOIN invoice as  i On o.invoice_id = i.invoice_id
 LEFT JOIN  Discount as D On o.discount_ID = d.discount_ID
 GROUP BY Name
 ORDER BY Revenue DESC
-Limit 3;
+LIMIT 3;
