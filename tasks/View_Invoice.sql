4cc01fecd98b53bf9eb1f3de7486690a370f4624
Ze <zepedro4259@gmail.com>
2021-10-23 17:01:27 +0100

    changed table name's first letter to uppercase

--- tasks/View_Invoice.sql
+++ tasks/View_Invoice.sql
@@ -1,12 +1,12 @@
-use fancy_company;
-CREATE VIEW INVOICE_VIEW
-AS 
-SELECT I.Invoice_ID AS 'Invoice Number', I.Created_At as 'Date of issue', concat(U.User_First_Name,' ', U.User_Last_Name) as 'Client Name', UA.User_Address_Streetname AS 'Street Address', UA.User_Address_City as 'City', UA.User_Address_Postalcode as 'Postal Code', UA.User_Adress_Country as 'Country', I.Sales_Amount AS 'Subtotal', (I.Sales_Amount*0.19)  AS 'Tax', (I.Sales_Amount*1.19) - (I.Sales_Amount*1.19* (D.Discount_Percentage/100)) as 'Total', D.Discount_Percentage AS 'Discount %'
-FROM invoice AS I, ORDERS AS O, user AS U, user_address AS UA, discount AS D
-WHERE I.Order_ID = O.Order_ID AND U.User_ID = O.Order_ID AND U.User_ID = UA.User_ID AND O.Discount_ID = O.Discount_ID;
+USE fancy_company;
 
-SELECT * FROM INVOICE_VIEW
-
-
-
-
+Select I.Invoice_ID AS 'Invoice Number', I.Created_At as 'Date of issue', DATE(DATE_ADD(I.created_at, INTERVAL 5 DAY)) AS 'Term',concat(U.User_First_Name,' ', U.User_Last_Name) as 'Client Name', UA.User_Address_Streetname AS 'Street Address', UA.User_Address_City as 'City', UA.User_Address_Postalcode as 'Postal Code', UA.User_Adress_Country as 'Country', I.Sales_Amount AS 'Subtotal', (I.Sales_Amount*0.19)  AS 'Tax', (I.Sales_Amount*1.19) - (I.Sales_Amount*1.19* (D.Discount_Percentage/100)) as 'Total', D.Discount_Percentage AS 'Discount %'
+from Orders AS O
+JOIN Invoice AS I
+ON O.Order_ID = I.Order_ID
+LEFT JOIN Discount as D 
+ON O.Discount_ID = D.Discount_ID
+LEFT JOIN User AS U
+ON U.User_ID = O.User_ID
+LEFT JOIN User_Address AS UA
+ON U.User_ID = UA.User_ID;
